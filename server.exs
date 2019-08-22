3318b5adaf47fbbd5c4e59dca1e70ebe9b9a786f
Luke Rodgers <lukeasrodgers@gmail.com>
2019-08-21 21:53:04 -0400

    half-assed spawn link and sig trappring
    a second acceptor is not linked, presumably because it would need to be
    linked to PID of parent, not PID of self here

--- server.exs
+++ server.exs
@@ -1,9 +1,16 @@
 defmodule TcpEchoServer do
 
   def start_server(port) do
+    Process.flag(:trap_exit, true)
+    IO.puts("starting server")
+    IO.inspect(self())
+
+    # spawn_link(fn -> sighandler() end)
+
     case :gen_tcp.listen(port, [:binary, {:active, false}]) do
       {:ok, listen_socket} -> 
-        spawn(fn -> acceptor(listen_socket) end)
+        spawn_link(fn -> acceptor(listen_socket) end)
+        sighandler()
         Process.sleep(:infinity)
       {:error, :eaddrinuse} ->
         IO.puts "address in use"
@@ -11,12 +18,27 @@ defmodule TcpEchoServer do
   end
 
   def acceptor(listen_socket) do
+    IO.puts("starting acceptor")
+    IO.inspect(self())
     # this will block, I think?
     {:ok, accept_socket} = :gen_tcp.accept(listen_socket)
-    # span same func to wait for other clients
-    spawn(fn -> acceptor(listen_socket) end)
+    # spawn same func to wait for other clients
+    spawn_link(fn -> acceptor(listen_socket) end)
 
     handle(accept_socket)
+  end
+
+  def sighandler do
+    IO.puts "sighandler"
+    receive do
+      {:EXIT, from, reason} ->
+        IO.puts("exit")
+        IO.inspect(from)
+        IO.puts("reason")
+        IO.inspect(reason)
+      {:normal, from, reason} ->
+        IO.puts("recieved normal exit" <> from <> "reason: " <> reason)
+    end
   end
 
   def handle(accept_socket) do
